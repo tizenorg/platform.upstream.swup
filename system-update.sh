f896a23e1d377c2f05aa921c4b18056be487dc23
William Douglas <william.douglas@intel.com>
2013-07-08 13:18:49 -0700

    Have swup logging go to the journal
    Change-Id: Ice577e897745118ff247ee5da111d77e66974291
    Signed-off-by: William Douglas <william.douglas@intel.com>

--- system-update.sh
+++ system-update.sh
@@ -11,7 +11,7 @@ function system_update
 	for i in `ls /var/cache/updatemanager/install`; do
 		UPDATE=$(echo $i | sed -e 's/^[0-9]*-//')
 		mkdir -p /var/cache/zypp/packages/$UPDATE/rpms
-		/usr/bin/swup -i  $UPDATE 2>&1 | tee /var/log/system-update.log
+		/usr/bin/swup -i  $UPDATE
 		rm /var/cache/updatemanager/install/$i
 	done
 
